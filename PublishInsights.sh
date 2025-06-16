08a7cb177569c438403f7370bf24ca86a93e950f
Kevin Wamba <disaya.bxl@gmail.com>
2025-06-16 21:56:53 +0200

    2nd update publish

--- PublishInsights.sh
+++ PublishInsights.sh
@@ -1,6 +1,6 @@
 export AWS_DEFAULT_REGION=eu-west-3
 export ACCOUNT_ID=116163866851
 export REPO_NAME=vibeconnect-insights-ai
-export IMAGE_NAME=directus
+export IMAGE_NAME=insightsAI
 export IMAGE_TAG=InsightsAI-DEVELOPMENT-1
 export ECR_URL=$ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com
